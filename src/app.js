45f85f06b06414fd1fb08320a06032194aa4f8c6
Viliam Elischer <viliam.elischer@zoomint.com>
2016-04-23 22:46:32 +0200

    increment & decrement by 2

--- src/app.js
+++ src/app.js
@@ -13,6 +13,14 @@ window.onload = function() {
     counterEl.innerText = counterValue;
   };
 
+  var incrementByTwoButton = document.createElement('button');
+  incrementByTwoButton.innerText = 'increment by 2';
+  incrementByTwoButton.onclick = function() {
+    counterValue += 2;
+    var counterEl = document.getElementById('counter');
+    counterEl.innerText = counterValue;
+  };
+
   var decrementButton = document.createElement('button');
   decrementButton.innerText = 'decrement';
   decrementButton.onclick = function() {
@@ -21,7 +29,17 @@ window.onload = function() {
     counterEl.innerText = counterValue;
   };
 
+  var decrementByTwoButton = document.createElement('button');
+  decrementByTwoButton.innerText = 'decrement by 2';
+  decrementByTwoButton.onclick = function() {
+    counterValue -= 2;
+    var counterEl = document.getElementById('counter');
+    counterEl.innerText = counterValue;
+  };
+
   document.body.appendChild(counter);
   document.body.appendChild(incrementButton);
+  document.body.appendChild(incrementByTwoButton);
   document.body.appendChild(decrementButton);
+  document.body.appendChild(decrementByTwoButton);
 }
