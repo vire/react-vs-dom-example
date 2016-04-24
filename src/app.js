7a26eff10a70cd53898f6de06fd42e12adb449df
Viliam Elischer <viliam.elischer@zoomint.com>
2016-04-24 11:30:21 +0200

    3 counters

--- src/app.js
+++ src/app.js
@@ -1,52 +1,109 @@
 window.onload = function() {
-  var initialState = 0;
-  var counterValue = initialState;
+  var firstCounterInitialState = 0;
+  var secondCounterInitialState = 2;
+  var thirdCounterInitialState = 5;
+  var firstCounterValue = firstCounterInitialState;
+  var secondCounterValue = secondCounterInitialState;
+  var thirdCounterValue = thirdCounterInitialState;
 
-  var counter = document.createElement('div');
-  counter.innerText = counterValue;
-  counter.id = 'counter';
+  var firstCounterEl = document.createElement('div');
+  firstCounterEl.innerText = firstCounterValue;
+  firstCounterEl.className = 'first-counter';
+
+  var secondCounterEl = document.createElement('div');
+  secondCounterEl.innerText = secondCounterValue;
+  secondCounterEl.className = 'second-counter';
+
+  var thirdCounterEl = document.createElement('div');
+  thirdCounterEl.innerText = thirdCounterValue;
+  thirdCounterEl.className = 'third-counter';
 
   var incrementButton = document.createElement('button');
   incrementButton.innerText = 'increment';
   incrementButton.onclick = function() {
-    counterValue += 1;
-    var counterEl = document.getElementById('counter');
-    counterEl.innerText = counterValue;
+    var _firstCounterEl = document.getElementsByClassName('first-counter')[0];
+    var _secondCounterEl = document.getElementsByClassName('second-counter')[0];
+    var _thirdCounterEl = document.getElementsByClassName('third-counter')[0];
+
+    firstCounterValue += 1;
+    secondCounterValue += 1;
+    thirdCounterValue += 1;
+
+    _firstCounterEl.innerText = firstCounterValue;
+    _secondCounterEl.innerText = secondCounterValue;
+    _thirdCounterEl.innerText = thirdCounterValue;
   };
 
   var incrementByTwoButton = document.createElement('button');
   incrementByTwoButton.innerText = 'increment by 2';
   incrementByTwoButton.onclick = function() {
-    counterValue += 2;
-    var counterEl = document.getElementById('counter');
-    counterEl.innerText = counterValue;
+    var _firstCounterEl = document.getElementsByClassName('first-counter')[0];
+    var _secondCounterEl = document.getElementsByClassName('second-counter')[0];
+    var _thirdCounterEl = document.getElementsByClassName('third-counter')[0];
+
+    firstCounterValue += 2;
+    secondCounterValue += 2;
+    thirdCounterValue += 2;
+
+    _firstCounterEl.innerText = firstCounterValue;
+    _secondCounterEl.innerText = secondCounterValue;
+    _thirdCounterEl.innerText = thirdCounterValue;
   };
 
   var decrementButton = document.createElement('button');
   decrementButton.innerText = 'decrement';
   decrementButton.onclick = function() {
-    counterValue -= 1;
-    var counterEl = document.getElementById('counter');
-    counterEl.innerText = counterValue;
+    var _firstCounterEl = document.getElementsByClassName('first-counter')[0];
+    var _secondCounterEl = document.getElementsByClassName('second-counter')[0];
+    var _thirdCounterEl = document.getElementsByClassName('third-counter')[0];
+
+    firstCounterValue -= 1;
+    secondCounterValue -= 1;
+    thirdCounterValue -= 1;
+
+    _firstCounterEl.innerText = firstCounterValue;
+    _secondCounterEl.innerText = secondCounterValue;
+    _thirdCounterEl.innerText = thirdCounterValue;
   };
 
   var decrementByTwoButton = document.createElement('button');
   decrementByTwoButton.innerText = 'decrement by 2';
   decrementByTwoButton.onclick = function() {
-    counterValue -= 2;
-    var counterEl = document.getElementById('counter');
-    counterEl.innerText = counterValue;
+    var incrementValue = 2;
+    var _firstCounterEl = document.getElementsByClassName('first-counter')[0];
+    var _secondCounterEl = document.getElementsByClassName('second-counter')[0];
+    var _thirdCounterEl = document.getElementsByClassName('third-counter')[0];
+
+    firstCounterValue -= 2;
+    secondCounterValue -= 2;
+    thirdCounterValue -= 2;
+
+    _firstCounterEl.innerText = firstCounterValue;
+    _secondCounterEl.innerText = secondCounterValue;
+    _thirdCounterEl.innerText = thirdCounterValue;
   };
 
   var resetButton = document.createElement('button');
   resetButton.innerText = 'reset';
   resetButton.onclick = function() {
-    counterValue = initialState;
-    var counterEl = document.getElementById('counter');
-    counterEl.innerText = counterValue;
+    var _firstCounterEl = document.getElementsByClassName('first-counter')[0];
+    var _secondCounterEl = document.getElementsByClassName('second-counter')[0];
+    var _thirdCounterEl = document.getElementsByClassName('third-counter')[0];
+
+    firstCounterValue = firstCounterInitialState
+    secondCounterValue = secondCounterInitialState;
+    thirdCounterValue = thirdCounterInitialState;
+
+    _firstCounterEl.innerText = firstCounterValue;
+    _secondCounterEl.innerText = secondCounterValue;
+    _thirdCounterEl.innerText = thirdCounterValue;
   };
 
-  document.body.appendChild(counter);
+  // display
+  document.body.appendChild(firstCounterEl);
+  document.body.appendChild(secondCounterEl);
+  document.body.appendChild(thirdCounterEl);
+  // controls
   document.body.appendChild(incrementButton);
   document.body.appendChild(incrementByTwoButton);
   document.body.appendChild(resetButton);
