1a723b12d2a531f1e91505d4e46861da6567215b
Viliam Elischer <viliam.elischer@zoomint.com>
2016-04-26 23:04:20 +0200

    added proptypes checking

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 export default class Counter extends React.Component {
+
+  static propTypes = {
+    value: React.PropTypes.number.isRequired,
+  }
+
   render() {
     return <span>{this.props.value}</span>;
   }
